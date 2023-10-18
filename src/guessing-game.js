09775608ca5e908e17e652d168e4439f86691b94
Maryna <rimana.ca@gmail.com>
2023-10-18 19:24:53 -0400

    feat: solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.mid = Math.round((this.min + this.max) / 2);
+        return this.mid;
     }
 
-    lower() {
-
+    lower() {//-> search left side
+        this.max = this.mid;//now max = mid
     }
 
-    greater() {
-
+    greater() {//-> search right side
+       this.min = this.mid;//now min = mid
     }
 }
 
